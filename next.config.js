0ec0e2350d29133469193fbacaa5b6d082d78aa2
Eelco <eelco.bosklopper@capgemini.com>
2022-03-04 15:02:48 +0100

    Added strict CSP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+`;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
